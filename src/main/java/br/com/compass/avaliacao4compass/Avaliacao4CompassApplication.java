676d52cba920bbb597fe496b46737325218c5a05
Heloisa Hobold Barbosa <heloisa.barbosa.pb@compasso.com.br>
2022-07-13 15:52:52 -0400

    Código base para a aplicação spring

---
+++ src/main/java/br/com/compass/avaliacao4compass/Avaliacao4CompassApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.avaliacao4compass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Avaliacao4CompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Avaliacao4CompassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compass/avaliacao4compass/Avaliacao4CompassApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.avaliacao4compass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Avaliacao4CompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
