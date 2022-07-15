a3c14e56ab9445aaa290d0dee69e517afbaddde0
Heloisa Hobold Barbosa <heloisa.barbosa.pb@compasso.com.br>
2022-07-15 16:01:10 -0400

    Inserindo arquivo data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO associate_entity (nome, cargo_politico, data_nascimento, sexo) VALUES ('José Henrique Castro', 'Deputado Estadual', '1968-08-15', 'Masculino');
+INSERT INTO associate_entity (nome, cargo_politico, data_nascimento, sexo) VALUES ('Fábio Filho Lima', 'Vereador', '1971-02-06', 'Masculino');
+INSERT INTO associate_entity (nome, cargo_politico, data_nascimento, sexo) VALUES ('Valquiria Neto Santos', 'Prefeito', '1978-11-22', 'Feminino');
+INSERT INTO associate_entity (nome, cargo_politico, data_nascimento, sexo) VALUES ('Lídia Ferreira', 'Vereador', '1985-03-01', 'Feminino');
+INSERT INTO associate_entity (nome, cargo_politico, data_nascimento, sexo) VALUES ('Antônio Figueira Silva', 'Deputado Federal', '1971-04-28', 'Masculino');
+
+INSERT INTO political_party_entity (nome, sigla, ideologia, data_fundacao) VALUES ('Partido dos Trabalhadores', 'PT', 'Esquerda', '1988-05-13');
+INSERT INTO political_party_entity (nome, sigla, ideologia, data_fundacao) VALUES ('Partido Socialista Brasileiro', 'PSB', 'Esquerda', '1998-10-08');
+INSERT INTO political_party_entity (nome, sigla, ideologia, data_fundacao) VALUES ('Partido da Social Democracia Brasileira', 'PSDB', 'Centro', '1990-09-27');
+
